76f589f0dd1b8286a9399a832dacb6cff84379d9
Steve-GE <estebanguzman970@gmail.com>
2022-06-07 15:59:36 -0500

    Subida de proyecto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-
-import { saludar } from './js/componentes';
 import './styles.css';
+import {Todo, TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
 
-const nombre = 'Esteban';
 
-saludar(nombre);
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+
+// const newTodo = new Todo('Aprender JS');
+// todoList.nuevoTodo(newTodo);
+
+console.log(todoList);
+
+
+
+
+
+
+// const tarea = new Todo('Aprender JS');
+// todoList.nuevoTodo(tarea);
+
+// console.log(todoList);
+
+// crearTodoHtml(tarea);
+
+
+// localStorage.setItem( 'mi-key', 'ABC1234' );
+
+// sessionStorage.setItem( 'mi-key', 'ABC1234' );
+
+// setTimeout( () => {
+//     localStorage.removeItem('mi-key');
+// }, 1500);
